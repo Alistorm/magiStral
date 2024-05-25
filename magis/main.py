7787e8a2cbae300eff77ade3b5072b2591024382
Alistorm <ali@upfundpro.com>
2024-05-25 12:40:57 +0200

    Added Gradio Guys

--- magis/main.py
+++ magis/main.py
@@ -1,16 +1,29 @@
-import getpass
-import os
 from datetime import datetime
 
+import gradio
+from gradio.server_messages import BaseMessage
 from langchain_core.messages import SystemMessage, AIMessage, HumanMessage
 from langchain_groq import ChatGroq
 
+chat = ChatGroq(temperature=0)
+messages: list[SystemMessage | AIMessage | HumanMessage] = [
+    SystemMessage(f"You are a helpful agent called Jarvis.Today is {datetime.now().isoformat()}."),
+    AIMessage("Hello dude my name is Jarvis! How can I help ?")
+]
+
+
+def query(message, history):
+    messages.append(HumanMessage(message['text']))
+    response = chat.invoke(messages)
+    if response.type == 'ai':
+        messages.append(AIMessage(response.content))
+    return messages[-1].content
+
+
 if __name__ == "__main__":
-    chat = ChatGroq(temperature=0)
-    messages = [
-        SystemMessage(f"You are a helpful agent called Jarvis.Today is {datetime.now().isoformat()}."),
-        AIMessage("Hello dude my name is Jarvis! How can I help ?")
-    ]
-    while True:
-        messages.append(HumanMessage(input(messages[-1].content)))
-        messages.append(chat.invoke(messages))
+    demo = gradio.ChatInterface(
+        fn=query,
+        title="magiStral",
+        multimodal=True,
+    )
+    demo.launch()
