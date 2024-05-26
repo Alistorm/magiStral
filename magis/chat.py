572f93e6c73e2bf9cd8b0e975f611548884bb308
Alistorm <ali@upfundpro.com>
2024-05-26 11:33:18 +0200

    Added Agents Guys

--- magis/chat.py
+++ magis/chat.py
@@ -1,7 +1,8 @@
+import json
 from datetime import datetime
 
 from mistralai.client import MistralClient
-from mistralai.models.chat_completion import ChatMessage
+from mistralai.models.chat_completion import ChatMessage, FunctionCall
 from orjson import orjson
 
 from magis.tools.main_tools import agent_creator_tool, agent_caller_tool
@@ -17,9 +18,11 @@ class LeMagisChat:
                 role='system',
                 content=f"""Today is {datetime.now()}.
                         You are a Magis helpful assistant with the ability to create and use expert agents to answer user needs.
-                        When you create an agent it is to enhance your capacity to answer best to the user needs.
+                        When you create an agent it should only be to enhance your capacity to give the best answer to user needs.
                         Once you created an agent you can use the agent to answer the user.
-                        Choose the agent if you think he has the expertise or to had context to your answer.
+                        You can created multiple agent at once.
+                        Choose the agents base on their expertise or to had context to your answer.
+                        You can update an agent by recalling the create function with the same agent_name.
                         """)
         ]
         self.tool_by_name = {}
@@ -34,16 +37,25 @@ class LeMagisChat:
             self.messages.append(ChatMessage(role="user", content=message))
             response = self.client.chat(model=self.model, messages=self.messages, tools=self.tools, tool_choice="auto")
             self.messages.append(response.choices[0].message)
-            if response.choices[0].message.tool_calls:
-                for tool_call in response.choices[0].message.tool_calls:
-                    name = tool_call.function.name
-                    params = orjson.loads(tool_call.function.arguments)
-                    result = self.tool_by_name[name](**params)
-                    self.messages.append(ChatMessage(role="tool", name=name, content=result, tool_call_id=tool_call.id))
-                response = self.client.chat(model=self.model, messages=self.messages, tools=self.tools,
-                                            tool_choice="auto")
+            tool_calls = response.choices[0].message.tool_calls
+            if tool_calls:
+                for tool_call in tool_calls:
+                    result = self.call_tool(tool_call.function)
+                    self.messages.append(
+                        ChatMessage(
+                            role="tool", tool_call_id=tool_call.id, name=tool_call.function.name, content=result))
+                response = self.client.chat(
+                    model=self.model, messages=self.messages, tools=self.tools, tool_choice="auto")
                 self.messages.append(response.choices[0].message)
-            # Extract and return response content
-            return response.choices[0].message.content
+            return self.messages[-1].content
 
         return "Unable to get request from chat, try again..."
+
+    def call_tool(self, fn: FunctionCall):
+        try:
+            if fn.arguments:
+                params = orjson.loads(fn.arguments)
+                result = self.tool_by_name.get(fn.name)(**params)
+                return result
+        except Exception as e:
+            return json.dumps({'error': str(e)})
